267263fcdf7d9adb63f678bfdeb587550f55bfaf
praisemwangi <fredmwangi084@gmail.com>
2024-02-22 15:52:27 +0300

    Add a new line in IntelliJ

---
+++ Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+    public static void main(String[] args){
+        /*Comments in Java
+        * We have three types of comments in Java
+        * 1. C++ Style comments - //
+        * 2. C style comments
+        * 3. special javadoc comments */
+
+        // 1. C++ comment style
+        System.out.println("1. We use // for C++ comment style");
+
+        /* 2. C comment style */
+        System.out.println("2. We use /**   for C comment style");
+
+        /**
+         *  3. Special javadoc comment
+         */
+        System.out.println("3. We use /** */ for special javadoc comment");
+    }
+}
